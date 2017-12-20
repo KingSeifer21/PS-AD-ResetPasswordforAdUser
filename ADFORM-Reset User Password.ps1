47b67d28698231843d4b4d79a0e19c2b09ab98d5
Amanuyl <amanuyl@hotmail.com>
2017-12-20 13:29:35 +0100

    Added Powershell Script

---
+++ ADFORM-Reset User Password.ps1
@@ -0,0 +1,11 @@
+﻿#This script will 
+#1-Ask for the username (please check you username policy)
+#2-It will aks to type in a new password for the user, please note that you have to verify the password (type it in twice)
+#Written by KingSeifer21 System Administrator\DevOps - Last update 18-12-2017
+
+#Start Script
+
+$User = Read-Host -Prompt 'Input username of whom you want to change the password'
+Get-AdUser -Identity $User
+$DName = Get-ADUser -Identity $User -Properties "DistinguishedName"
+Set-ADAccountPassword -Identity $DName -Reset
