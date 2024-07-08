704066c0bc3f518401eb9d9497da491354801a6b
KingSeifer21 <34708640+KingSeifer21@users.noreply.github.com>
2024-07-08 20:33:42 +0200

    Update ADFORM-Reset User Password.ps1

--- ADFORM-Reset User Password.ps1
+++ ADFORM-Reset User Password.ps1
@@ -1,7 +1,7 @@
 ﻿#This script will 
 #1-Ask for the username (please check you username policy)
 #2-It will aks to type in a new password for the user, please note that you have to verify the password (type it in twice). Please also note here you Password Policy..
-#Written by KingSeifer21 System Administrator\DevOps - Last update 18-12-2017
+#Written by Amanuyl Ok System Administrator\DevOps - Last update 18-12-2017
 
 #Start Script
 
